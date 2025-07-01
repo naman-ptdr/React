f98b7ce4665c86f51de40a4ececce3af9b8236e6
Naman_ptdr <namanpatidar5555@gmail.com>
2025-07-01 17:24:00 +0530

    Context Api project 2 dark light mode change

--- 08minicontext/src/Context/UserContext.js
+++ 08minicontext/src/Context/UserContext.js
@@ -1,5 +1,6 @@
 import React from "react"
 
-const UserContext = React.createContext();
+const UserContext = React.createContext();  
+// Here No need to write React as we can see we import React already if we not done then only we have to do
 
 export default UserContext;
